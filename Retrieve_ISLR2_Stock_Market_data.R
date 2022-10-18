e8b6b66da1b62194c8d3ba3765af61d7abef98ec
Caglar Onal <caglar1900@gmail.com>
2022-10-17 23:15:10 -0500

    Add files via upload

---
+++ Retrieve_ISLR2_Stock_Market_data.R
@@ -0,0 +1,4 @@
+library ( ISLR2 )
+names ( Smarket )
+Smarket -> stock_market_data
+write.csv(stock_market_data,"C:/Users/onal_/Study/Machine Learning/Linear Discriminant Analysis//stock_market_data.csv", row.names = FALSE)
